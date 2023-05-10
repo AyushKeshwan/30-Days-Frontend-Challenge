bc5d3ff6985ec2a382bf647a863eb38b6e686640
Ayush Keshwan <98636040+AyushKeshwan@users.noreply.github.com>
2023-05-10 22:51:55 +0530

    Add files via upload
    Today, I delved into the fascinating world of JavaScript, focusing on data types and variables. Understanding the different data types, such as strings, numbers, and booleans, empowers me to build dynamic and interactive web applications. Excited to continue this JavaScript journey and unlock even more possibilities.

---
+++ Day 9/Practice.js
@@ -0,0 +1,56 @@
+// Day 9 of 30
+
+// 1. Declare a variable named "name" and assign it a string value containing your name. Print the value of the variable to the console.
+
+let intro = "Ayush Keshwan";
+console.log(`Hi My name is ${intro}`);
+// Output - Hi My name is Ayush Keshwan
+
+
+// 2. Create a variable called "age" and assign it a number representing your age. Calculate the age in months and print the result.
+
+let age = 21;
+let ageInMonths = age * 12;
+console.log(`You age in months is ${ageInMonths} months!`);
+// Output - You age in months is 252 months!
+
+
+// 3. Declare a variable called "isStudent" and assign it a boolean value indicating whether you are currently a student or not. Print the value to the console.
+
+let isStudent = true;
+console.log(isStudent);
+// Output - true
+
+
+// 4. Create two variables, "num1" and "num2," and assign them numeric values. Perform addition, subtraction, multiplication, and division operations using these variables and print the results.
+
+let num1 = 10;
+let num2 = 5;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log(addition);
+// Output - 15
+
+console.log(subtraction);
+// Output - 5
+
+console.log(multiplication);
+// Output - 50
+
+console.log(division);
+// Output - 2
+
+
+// 5. Declare a variable called "myArray" and assign it an array of your favorite fruits. Print the length of the array and the second element in the array.
+
+let myArray = ["apple", "banana", "orange"];
+
+console.log(myArray.length);
+// Output - 3
+
+console.log(myArray[1]);
+// Output - banana
